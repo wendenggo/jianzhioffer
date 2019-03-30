f42838263a815da5c4736a39d4962323e7bc62af
zwdeng <zwdeng@163.com>
2019-03-30 09:26:35 +0800

    first

---
+++ src/MyPower/Solution.java
@@ -0,0 +1,16 @@
+package MyPower;
+
+public class Solution {
+    public double Power(double base, int exponent) {
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        double a = 0;
+        double b = 0;
+        float c = 0;
+        float d = 0;
+        System.out.println(a==b);
+        System.out.println(c==d);
+    }
+}
